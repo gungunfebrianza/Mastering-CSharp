362cdbc8a20b6e8f1cd163c36f1319d8014c9d6f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-04-05 05:52:45 +0700

    Add Type Inference

---
+++ Fundamental/TypeInference/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TypeData
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var age = 25;
+      int score = 100;
+      Type ageType = age.GetType();
+      Console.WriteLine($"age is {ageType}"); //age is System.Int32
+      Console.WriteLine($"score is {score.GetType()}"); //score is System.Int32
+    }
+  }
+}
