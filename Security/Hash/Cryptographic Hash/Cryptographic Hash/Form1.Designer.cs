aa354957c3fd16b08bb19cf52dabe2bba6853135
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-04 05:29:10 +0700

    Add Hash Example

---
+++ Security/Hash/Cryptographic Hash/Cryptographic Hash/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Cryptographic_Hash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hash";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(482, 121);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Text";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(469, 88);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 149);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(482, 126);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Message Digest";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 22);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(469, 98);
+            this.textBox2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Location = new System.Drawing.Point(500, 22);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(259, 93);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Salt";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 22);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(246, 61);
+            this.textBox3.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Controls.Add(this.button2);
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Location = new System.Drawing.Point(507, 193);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(252, 82);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Operation";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(89, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Verify";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.comboBox1);
+            this.groupBox5.Location = new System.Drawing.Point(507, 121);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(252, 66);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Hashing Algorithm";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SHA256",
+            "SHA384",
+            "SHA512"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(239, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "SHA384";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(171, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 44);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 290);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(396, 20);
+            this.toolStripStatusLabel1.Text = "Developed By Gun Gun Febrianza for Educational purpose.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 316);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Cryptographic Hash";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Security/Hash/Cryptographic Hash/Cryptographic Hash/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Cryptographic_Hash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SHA384 sha384Hash = SHA384.Create())
+            {
+                //From String to byte array
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(textBox1.Text);
+                byte[] hashBytes = sha384Hash.ComputeHash(sourceBytes);
+                string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+				textBox2.Text = ComputeHash(textBox1.Text, comboBox1.Text, Encoding.ASCII.GetBytes(textBox3.Text));
+            }
+
+        }
+
+
+		public static string ComputeHash(string plainText, string hash, byte[] salt)
+		{
+			int minSaltLength = 4, maxSaltLength = 16;
+
+			byte[] saltBytes = null;
+			if (salt != null)
+			{
+				saltBytes = salt;
+			}
+			else
+			{
+				Random r = new Random();
+				int SaltLength = r.Next(minSaltLength, maxSaltLength);
+				saltBytes = new byte[SaltLength];
+				RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+				rng.GetNonZeroBytes(saltBytes);
+				rng.Dispose();
+			}
+
+			byte[] plainData = ASCIIEncoding.UTF8.GetBytes(plainText);
+			byte[] plainDataWithSalt = new byte[plainData.Length + saltBytes.Length];
+
+			for (int x = 0; x < plainData.Length; x++)
+				plainDataWithSalt[x] = plainData[x];
+			for (int n = 0; n < saltBytes.Length; n++)
+				plainDataWithSalt[plainData.Length + n] = saltBytes[n];
+
+			byte[] hashValue = null;
+
+			switch (hash)
+			{
+				case "SHA256":
+					SHA256Managed sha = new SHA256Managed();
+					hashValue = sha.ComputeHash(plainDataWithSalt);
+					sha.Dispose();
+					break;
+				case "SHA384":
+					SHA384Managed sha1 = new SHA384Managed();
+					hashValue = sha1.ComputeHash(plainDataWithSalt);
+					sha1.Dispose();
+					break;
+				case "SHA512":
+					SHA512Managed sha2 = new SHA512Managed();
+					hashValue = sha2.ComputeHash(plainDataWithSalt);
+					sha2.Dispose();
+					break;
+			}
+
+			byte[] result = new byte[hashValue.Length + saltBytes.Length];
+			for (int x = 0; x < hashValue.Length; x++)
+				result[x] = hashValue[x];
+			for (int n = 0; n < saltBytes.Length; n++)
+				result[hashValue.Length + n] = saltBytes[n];
+
+			return Convert.ToBase64String(result);
+		}
+
+		public static bool Confirm(string plainText, string hashValue, string hash, byte[] salt)
+		{
+			byte[] hashBytes = Convert.FromBase64String(hashValue);
+			int hashSize = 0;
+
+			switch (hash)
+			{
+				case "SHA256":
+					hashSize = 32;
+					break;
+				case "SHA384":
+					hashSize = 48;
+					break;
+				case "SHA512":
+					hashSize = 64;
+					break;
+			}
+
+			byte[] saltBytes = new byte[hashBytes.Length - hashSize];
+
+			for (int x = 0; x < saltBytes.Length; x++)
+				saltBytes[x] = hashBytes[hashSize + x];
+
+			string newHash = ComputeHash(plainText, hash, saltBytes);
+
+			return (hashValue == newHash);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			bool result = Confirm(textBox1.Text, textBox2.Text, comboBox1.Text, Encoding.ASCII.GetBytes(textBox3.Text));
+			if (result == true)
+			{
+				MessageBox.Show("Authentic!", "Cryptographic Hash", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			} else
+			{
+				MessageBox.Show("Not Authentic!", "Cryptographic Hash", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			textBox1.Clear();
+			textBox2.Clear();
+			textBox3.Clear();
+		}
+	}
+}
